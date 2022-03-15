c1884d1d8c0bcc615cf37e86b1babeb71c00347b
Karolina Kopacz <karolina.kopacz94@gmail.com>
2022-03-15 14:23:28 +0100

    add styles to chat screen

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-import CardHeader from "react-bootstrap/esm/CardHeader";
-import { Card } from "react-bootstrap";
-
 import { getData } from "../actions/actions";
 import { Data } from "./types";
 
@@ -20,30 +17,70 @@ const ChatMessages = () => {
       {data
         ? data.map((user) => {
             return (
-              <>
-                <Card
-                  border="primary"
-                  // use className instead of line styles
-                  style={{ width: "50%", height: "200px", margin: "auto" }}
-                >
-                  <Card.Img
-                    variant="top"
-                    src={`${user.avatar}/100px160`}
-                    style={{ width: "20%", display: "flex", margin: "auto" }}
-                  />
-
-                  <CardHeader>
+              <div style={styles.box}>
+                <div style={styles.imgBox}>
+                  <div>
+                    <img style={styles.img} src={`${user.avatar}`}></img>
+                  </div>
+                </div>
+                <div style={styles.textBox}>
+                  <div style={styles.userName}>
                     {user.first_name} {user.last_name}
-                  </CardHeader>
-
-                  <Card.Body>{user.message}</Card.Body>
-                </Card>
-              </>
+                  </div>
+                  <div style={styles.message}>{user.message}</div>
+                </div>
+              </div>
             );
           })
         : null}
     </>
   );
+};
+
+const styles: {
+  box: React.CSSProperties;
+  imgBox: React.CSSProperties;
+  img: React.CSSProperties;
+  textBox: React.CSSProperties;
+  userName: React.CSSProperties;
+  message: React.CSSProperties;
+} = {
+  box: {
+    width: "90%",
+    height: "7%",
+    padding: "10px 0",
+    margin: "10px auto",
+    display: "flex",
+    flexDirection: "row",
+  },
+  imgBox: {
+    margin: "auto 15px 0 0px",
+    display: "flex",
+  },
+  img: {
+    width: "50px",
+    height: "50px",
+    border: "1px solid rgb(174, 194, 227)",
+    borderRadius: "50%",
+  },
+  textBox: {
+    padding: "1.5%",
+    maxWidth: "100%",
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "rgb(174, 194, 227)",
+    justifyContent: "flex-start",
+    borderRadius: "15px",
+  },
+  userName: {
+    display: "flex",
+    justifyContent: "flex-start",
+    fontSize: "12px",
+  },
+  message: {
+    display: "flex",
+    textAlign: "start",
+  },
 };
 
 export default ChatMessages;
--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -3,11 +3,22 @@ import ChatMessages from "../components/ChatMessages";
 
 const ChatScreen = () => {
   return (
-    <>
+    <div style={styles.chatWindow}>
       <div>Strona czatu</div>
       <ChatMessages />
-    </>
+    </div>
   );
+};
+
+const styles = {
+  chatWindow: {
+    margin: "auto",
+    marginTop: "30px",
+    width: "75%",
+    height: "80%",
+    border: "1px solid rgb(122, 167, 240) ",
+    borderRadius: "10px",
+  },
 };
 
 export default ChatScreen;
